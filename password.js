265ef5293cf70612aeedbb95a1dea6331a3c5f1a
Mitya Kim <mitya_kim@icloud.com>
2021-11-12 12:42:07 -0800

    Last commit

--- password.js
+++ password.js
@@ -13,7 +13,7 @@ reader.question("Please, enter your password", function(input){
     if (str === 10 && input.match(upperCaseLetters)){
         console.log('\x1b[36m%s\x1b[0m', 'Hi, \n' + 'how are you?')
     } else {
-        console.log('%Wrong \n' + 'password!','color:red;', 'color: blue;')
+        console.log('Wrong \n' + 'password!')
     }
     
   reader.close()
