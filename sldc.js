38c152ab4b5201fe72d3f076fa2a1d11a2eb6c3f
Mitya Kim <mitya_kim@icloud.com>
2021-11-12 16:14:45 -0800

    Final commit

--- sldc.js
+++ sldc.js
@@ -18,9 +18,9 @@
     values of each variable to a definition of the step.
 */
 
-let analysisOfRequirements = 1
-let planning = 2
-let design = 3
-let implementation = 4
-let testingIntegration = 5
-let maintain = 6
+let planning = "The planning stage is exactly what it sounds like: the phase in which developers will plan for the upcoming project."
+let analysisOfRequirements = "The analysis stage includes gathering all the specific details required for a new system as well as determining the first ideas for prototypes."
+let design = "The design stage is a necessary precursor to the main developer stage. Developers will first outline the details for the overall application, alongside specific aspects."
+let implementation = "The implementation stage is the part where developers actually write code and build the application according to the earlier design documents and outlined specifications."
+let testingIntegration = "Now it must be tested to make sure that there aren’t any bugs and that the end-user experience will not negatively be affected at any point. Different modules or designs will be integrated into the primary source code through developer efforts, usually by leveraging training environments to detect further errors or defects."
+let maintain = "Developers are responsible for implementing any changes that the software might need after deployment."
